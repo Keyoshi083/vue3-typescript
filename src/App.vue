716c02abc821863393bdb49c9fc10805e316962e
Keyoshi083 <gt.chicken1@gmail.com>
2022-07-21 17:34:29 +0900

    slotについて実装

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from "./components/HelloWorld.vue";
-import Payment from "./components/Payment.vue";
-import Tweet from "./components/Tweet.vue";
-import Persons from "./components/Persons.vue";
+import CardList from './components/CardList.vue';
 </script>
 
 <template>
-  <Persons />
+  <CardList></CardList>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-.label {
-  color: red;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="card-container">
+    <span class="title">{{ title }}</span>
+    <!-- 親コンポの内部で指定されたものを表示する（slot) -->
+    <slot></slot>
+  </div>
+  <div class="footer">
+    <slot name="footer"><span>Default Footer</span></slot>
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  box-shadow: 2, 8px, black;
+  border-radius: 8px 8px 0px 0px;
+  padding: 20px 20px;
+  width: 300px;
+  height: 80px;
+  background-color: #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 600;
+}
+
+.footer {
+  display: flex;
+  height: 30px;
+  background-color: aquamarine;
+  width: 100%;
+  border-radius: 0px 0px 8px 8px;
+  justify-content: center;
+  align-items: center;
+}
+</style>
